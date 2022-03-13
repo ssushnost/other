619cf74b5ff09db73b79051f0f444a75a31fadaf
ssushnost <98917669+ssushnost@users.noreply.github.com>
2022-03-14 00:21:28 +0300

    Add files via upload

--- constants.py
+++ constants.py
@@ -3,7 +3,18 @@ eng_alph_upper = eng_alph.upper()
 rus_alph = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 rus_alph_upper = rus_alph.upper()
 other = '`1234567890-=~!@#$%^&*()_+][}{\';":/?\\.>,< '
-labels = {'encrypted_text': {'rus': 'зашифрованный текст:', 'eng': 'encrypted text:'}}
+labels = {'encrypted_text_label': {'rus': 'зашифрованный текст:', 'eng': 'encrypted text:'},
+          'encrypted_text': {'rus': 'ожидается ввод текста/ключа', 'eng': 'waiting for text/key'}}
+entrys = {'text_entry': {'rus': 'текст', 'eng': "text"},
+          'key_entry': {'rus': 'ключ', 'eng': 'key'}}
+menus = {'ciphers': {'rus': 'шифры', 'eng': 'ciphers'},
+         'caesar': {'rus': 'цезарь', 'eng': 'caesar'},
+         'atbash': {'rus': 'атбаш', 'eng': 'atbash'},
+         'language': {'rus': 'язык', 'eng': 'language'},
+         'russian': {'rus': 'русский', 'eng': 'russian'},
+         'english': {'rus': 'английский', 'eng': 'english'}}
+buttons = {'rus': {'rus': 'рус', 'eng': 'rus'},
+           'eng': {'rus': 'англ', 'eng': 'eng'}}
 alphabets = {
     'rus':
         {
