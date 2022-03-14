eaa33a23f0b1da59d8fa0c048d0e6d8efde6ff1f
ssushnost <98917669+ssushnost@users.noreply.github.com>
2022-03-14 10:15:04 +0300

    Create bst.js

---
+++ bst.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor(value,left=None,right=None,parent=None){
+        this.value = value
+        this.left = left
+        this.right = right
+        this.parent = parent
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        self.root=None
+        
+    }
+}
